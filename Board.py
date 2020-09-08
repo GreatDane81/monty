c3ea40fb42a93c41833c4da3cd50e5849938d804
GreatDane81 <ethan.dain@gmail.com>
2020-09-08 16:34:21 -0400

    implementing a new representation of the data for better pattern matching (hopefully)

--- Board.py
+++ Board.py
@@ -1,97 +1,107 @@
 import numpy as np
 # moved it all to class, have to test. Maybe clean up too.
 
-ROWS = 9 # 8 for rows, + 1 for storing state. Ugly, but necessary for keras.
-COLUMNS = 8 # one per col
-PIECES = 13 # 6 for white, 6 for black + 1 for empty/full bit. 
+ROWS = 8
+COLUMNS = 8
+LAYERS = 6 + 2 + 1 # 9: 6 for piece structure (one layer for each peice, 1/-1 for black/white), 2 for SQUARES_ATTACKED and HEURESTICS
 
-# Storing these in row 9, will be important to engine eval of a lot of positions.
-# Notice that this effectively only stores whether or not the king has moved,
-# otherwise legal_moves() takes care of the rest
+# PIECE LAYERS:
+KING_LAYER = 0
+PAWN_LAYER = 1
+QUEEN_LAYER = 2
+ROOK_LAYER = 3
+BISHOP_LAYER = 4
+KNIGHT_LAYER = 5 # which is the last layer
+LAST_LAYER = KNIGHT_LAYER
 
-# change: all stored at [8, x, 0], but this shift determines where exactly
-WHITE_KSIDE_CASTLE_SHIFT = 1 # will be at board[8,1,0]
-WHITE_QSIDE_CASTLE_SHIFT = 2 # will be at board[8,2,0]
-BLACK_KSIDE_CASTLE_SHIFT = 3 # will be at board[8,3,0]
-BLACK_QSIDE_CASTLE_SHIFT = 4 # will be at board[8,4,0]
-# pro tip, can use a PLAYER_TURN*6 + PIECE_SHIFT to address the pieces vector.
+#TURNS
+WHITES_TURN = 0
+BLACKS_TURN = 1
+
+# ATTACK LAYERS:
+WHITE_ATTACK_LAYER = 6
+BLACK_ATTACK_LAYER = 7
+
+# HEURISTICS LAYER
+HEURESTICS_LAYER = 8
+# HEURISTICS BIT SHIFTS
+TURN_INDEX = [0,0] # going to leave the rest of the row blank for pattern recognition
+WHITE_KSIDE_CASTLE_INDEX = [1,0] # has to be two dimensions becuase hereustics is now a layer
+WHITE_QSIDE_CASTLE_INDEX = [1,1]
+BLACK_KSIDE_CASTLE_INDEX = [1,2]
+BLACK_QSIDE_CASTLE_INDEX = [1,3]
+
+# PRESENT BITS
+WHITE_PRESENT = 1
+BLACK_PRESENT = -1
+
+# LAYER TO STR
+WHITE_LAYER_TO_PIECE = {KING_LAYER:'k', QUEEN_LAYER: 'q', ROOK_LAYER:'r', BISHOP_LAYER:'b', KNIGHT_LAYER: 'n', PAWN_LAYER: 'p'}
+BLACK_LAYER_TO_PIECE = {KING_LAYER:'K', QUEEN_LAYER: 'Q', ROOK_LAYER:'R', BISHOP_LAYER:'B', KNIGHT_LAYER: 'N', PAWN_LAYER: 'P'}
+
+# PIECE TO LAYER (all in one dict for useability in update_index)
+PIECE_TO_LAYER = {'k': KING_LAYER, 'q': QUEEN_LAYER, 'r': ROOK_LAYER, 'b': BISHOP_LAYER, 'n': KNIGHT_LAYER, 'p': PAWN_LAYER,
+                  'K': KING_LAYER, 'Q': QUEEN_LAYER, 'R': ROOK_LAYER, 'B': BISHOP_LAYER, 'N': KNIGHT_LAYER, 'P': PAWN_LAYER}
+                                                                                                                            
+# For promotion and conversion of types:
+PYCHESS_PIECE_TYPE_TO_BOARD_PIECE_TYPE = {1:'p', 2:'n', 3:'b', 4:'r', 5:'q', 6:'k'}
 
 # n for knight for disambuity, I'm sorry
-PIECE_OFFSET = {'p':1, 'n':2, 'b':3, 'r':4, 'q':5, 'k':6} # offset of 1 for pawn because of empty bit
-PIECE_TO_CHAR = {1:'p', 2:'n', 3:'b', 4:'r', 5:'q', 6:'k',7:'P', 8:'N', 9:'B', 10:'R', 11:'Q', 12:'K'}
 LETTER_TO_ROW = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4,'f':5, 'g':6,'h':7}
-PIECE_TO_BARRAY = {'p':1, 'n':2, 'b':3, 'r':4, 'q':5, 'k':6,'P':7, 'N':8, 'B':9, 'R':10, 'Q':11, 'K':12}
+
 
 class Board: 
-    
-
     def __init__(self):
         '''
         Creates a new board with standard starting position
         '''
-        self.board = np.zeros([ROWS, COLUMNS, PIECES],dtype='float32')
-        self.board[0:2, :, 0] = 1 # filling the first two rows with "full" bits (represents white)
-        self.board[6:8, :, 0] = 1 # filling the last two rows with "full bits" (represents black)
-        # Turn starts off as white, so no need to set the turn bit (board[8:0:0]) to 1.
-        # set the white pawn row
-        self.board[1, :, PIECE_OFFSET['p']] = 1
-        # set the black pawn row
-        self.board[6, :, 6 + PIECE_OFFSET['p']] = 1 # + 6 because they are black pawns
-        # set the back rows for both white and black
-        self.set_back_rank('w')
-        self.set_back_rank('b')
-        # Making sure both sides can castle:
-        self.board[8, WHITE_KSIDE_CASTLE_SHIFT, 0] = 1 # white_k_side
-        self.board[8, WHITE_QSIDE_CASTLE_SHIFT, 0] = 1 # white_q_side
-        self.board[8, BLACK_KSIDE_CASTLE_SHIFT, 0] = 1 # black k side
-        self.board[8, BLACK_QSIDE_CASTLE_SHIFT, 0] = 1 # black q side
-        self.move_stack = []
+        # start with an empty board
+        self.board = np.zeros([ROWS, COLUMNS, LAYERS],dtype='float32')
+        # set up piece layers
+        pieces_to_index = {'k':{'e1'}, 'q':{'d1'}, 'r':{'a1','h1'}, 'n':{'b1', 'g1'}, 'b':{'f1','c1'}, 'p':{'a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'},
+                           'K':{'e8'}, 'Q':{'d8'}, 'R':{'a8','h8'}, 'N':{'b8', 'g8'}, 'B':{'f8','c8'}, 'P':{'a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'}}
+        self.set_up_pieces(pieces_to_index)
+        # set up attack layers
+        #self.white_attack_setup()
+        #self.black_attack_setup()
+        # set up heurestics layer
+        #self.heurestics_setup()
 
-    def set_back_rank(self, colour):
-        if colour == 'w':
-            colour_offset = 0
-            row = 0
-            # Do king and queen separately
-            self.board[row, 3, PIECE_OFFSET['q'] + 6*colour_offset] = 1
-            self.board[row, 4, PIECE_OFFSET['k'] + 6*colour_offset] = 1
-        else:
-            colour_offset = 1
-            row = 7
-            self.board[row, 3, PIECE_OFFSET['q'] + 6*colour_offset] = 1
-            self.board[row, 4, PIECE_OFFSET['k'] + 6*colour_offset] = 1
-        # Take the first piece in the row, the rook, and find its index.
-        # if the colour is black, shift 6 down and set the correct bit to 1
-        self.board[row, 0, PIECE_OFFSET['r'] + 6*colour_offset] = 1
-        self.board[row, 1, PIECE_OFFSET['n'] + 6*colour_offset] = 1
-        self.board[row, 2, PIECE_OFFSET['b'] + 6*colour_offset] = 1
-        # did the middle rows above
-        self.board[row, 5, PIECE_OFFSET['b'] + 6*colour_offset] = 1
-        self.board[row, 6, PIECE_OFFSET['n'] + 6*colour_offset] = 1
-        self.board[row, 7, PIECE_OFFSET['r'] + 6*colour_offset] = 1
+    def set_up_pieces(self, pieces_to_index):
+        """
+        dict of (char: {char}), 'k': 'e1'
+    
+        To set up all the pieces in their appropriate layer
+        """
+        for piece in pieces_to_index:
+            # get the piece's list
+            piece_indices = pieces_to_index[piece]
+            for index in piece_indices: # for every index it appears in
+                index = Board.parse_index(index)
+                self.update_index(index[0], index[1], piece)
+
 
     def __str__(self):
         '''
         Converts the numpy board into a human readable string
         '''
-        #white_k_has_moved = board[8, 1, 0] & True # (anding for readability)
-        #black_k_has_moved = board[8, 2, 0] & True
-        #turn = board[8, 0, 0] & True
         s = ""
-        for row in range(0, 8):
-            s = self.row_to_str(row) + "\n" + s
+        for row in range(7, -1, -1):
+            s += self._print_row(row) + "\n"
         return s
-
-    def row_to_str(self, row_num):
+    
+    def _print_row(self, row):
         s = ""
         for col in range(0, 8):
-            if self.board[row_num, col, 0] == 0:
+            piece = Board.get_piece_from_index(self.board, row, col)
+            if piece == None:
                 s += "."
             else:
-                s += Board.get_piece_from_index(self.board, row_num, col)
+                s += piece
         return s
 
 
-    def get_board_tensor(self):
+    def get_constant_tensor(self):
         '''
         Numpy Board --> Constant tensor of the board
         '''
@@ -99,12 +109,11 @@ class Board:
 
     def play_move(self, move, promotion):
         '''
-        (Numpy, str) -> Numpy
-        Takes a Numpy board, updates it given a move from move in game.mainline_moves()
+        (str, pychess ChessPiece) -> None
+
+        Updates the board given coordinates for a move, promotes if necessary.
         '''
-        # TODO: Take care of special cases like promotion, castling, and maybe en passant.
-        # first, parse the move string
-        # TODO: This includes updating the bit map for castling.
+        # start by getting indices
         indices = Board.parse_move_indices(move)
         start_index = indices[0]
         target_index = indices[1]
@@ -128,70 +137,47 @@ class Board:
         # then, update the target index
         if promotion != None:
             # Then get the promotion piece
-            piece = PIECE_TO_CHAR[promotion]
+            py_chess_piece_type = promotion
+            piece = PYCHESS_PIECE_TYPE_TO_BOARD_PIECE_TYPE[py_chess_piece_type]
+            if self.get_turn() == BLACKS_TURN:
+                # then take it to uppercase, because it's black's turn
+                piece = piece.upper()
         self.update_index(target_index[0], target_index[1], piece)
         # And switch player turns
         self.update_turn()
-    
 
-    @staticmethod
-    def parse_move_indices(move):
-        '''
-        returns the index for start piece, target piece
-        --> [(s_row, s_col), (t_row, t_col)]
-        NOTE: only takes the first chars for the indices, promotions and mate not considered
-        '''
-        move_str = str(move)
-        start_sq = move_str[0:2]
-        target_sq = move_str[2:4]
-        # g1 should be 0, 6
-        start_index = Board.parse_index(start_sq)
-        target_index = Board.parse_index(target_sq)
-        return [start_index, target_index]
-    
-            
-    @staticmethod
-    def get_piece_from_index(board, row, col):
-        '''
-        (row, col) -> str
-
-        A1 -> 0, 0
-        TODO: Clean up this code, board should be an argument
-        '''
-        barray = board[row, col]
-        return Board.get_piece_from_bit_array(board, barray)
-    @staticmethod
-    def get_piece_from_bit_array(board, barray):
-        '''
-        bit array for pieces --> piece rep in str
-        '''
-        if barray[0] == 0:
-            return None # TODO: error
-        for k in range (1,13): 
-            if barray[k] == 1:
-                # found the piece
-                return PIECE_TO_CHAR[k]
 
     def empty_index(self, row, col):
         '''
         Empties the square at [row, col].
-        Intended for pieces only, don't apply to row 9.
+
+        Raises Exception if no piece at that index.
         '''
-        # set the bit array to 0 (selection bit will be empty, as will all pieces)
-        self.board[row,col] = np.zeros(PIECES)
-        return
+        # first, check there is a piece (error should propogate, so no try catch)
+        piece_layer = Board.get_piece_layer_from_index(self.board, row, col)
+        # get the piece
+        piece = self.board[piece_layer, row, col]
+        print(piece.shape)
+        #if piece == 0:
+        #    raise Exception("Tried to empty a non-existent piece at row,", row, "col,", col)
 
     def update_index(self, row, col, piece):
         '''
+        (int, int, char)
         Writes over the square at row, col, with 'piece'.
         Note: Old piece is wiped, this accounts for captures.
         '''
-        # Empty the board's pieces before updating
-        self.board[row, col] = np.zeros(PIECES)
-        # first, set the bit map to contain a piece
-        self.board[row, col, 0] = 1
-        # next, find the shift using PIECE_TO_SHIFT
-        self.board[row, col, PIECE_TO_BARRAY[piece]] = 1
+        # First, empty the target index to ensure no two layers save an overlapping index
+        # (I.E) both knight and bishop occupy a square
+        self.empty_index(row, col)
+        # next, find the appropriate layer to put in the new piece
+        layer = PIECE_TO_LAYER[piece]
+        if piece.islower():
+            # then update with WHITE_PRESENT
+            self.board[layer, row, col] = WHITE_PRESENT
+        else:
+            self.board[layer, row, col] = BLACK_PRESENT
+
 
     def castle_king_side(self, colour):
         '''
@@ -253,69 +239,68 @@ class Board:
         # update the pieces
         self.update_index(king_target[0], king_target[1], king_char)
         self.update_index(rook_target[0], rook_target[1], rook_char)
-        self.update_turn()
         # update castling bit
         if king_char == "k":
             self.set_white_no_castle("A")
         else:
             self.set_black_no_castle("A")
-
-    @staticmethod
-    def parse_index(index):
-        '''
-        "a1" --> (0, 0)
-        TODO: update to use in other functions that need it
-        '''
-        # TODO: Assert length = 2
-        return (int(index[1]) - 1, LETTER_TO_ROW[index[0]])
-
+        # conclude by updating the turn
+        self.update_turn()
 
     def update_turn(self):
-        self.board[8,0,0] = 1 - self.board[8,0,0]
+        """
+        Flips the turn bit
+        """
+        turn = self.board[HEURESTICS_LAYER,TURN_INDEX[0],TURN_INDEX[1]]
+        self.board[HEURESTICS_LAYER,TURN_INDEX[0],TURN_INDEX[1]] = 1 - turn
 
     def white_can_castle(self, side):
         """
+        (char) -> None
+
         A for either side
         K for kingside, Q for Q side
         """
+        kside = self.board[HEURISTICS_LAYER, WHITE_KSIDE_CASTLE_INDEX[0], WHITE_KSIDE_CASTLE_INDEX[1]] == 1
+        qside = self.board[HEURISTICS_LAYER, WHITE_QSIDE_CASTLE_INDEX[0], WHITE_QSIDE_CASTLE_INDEX[1]] == 1
         if side == "A":
-            # want to know if you can castle on either side
-            return self.board[8, WHITE_KSIDE_CASTLE_SHIFT, 0] == 1 or self.board[8, WHITE_QSIDE_CASTLE_SHIFT, 0] == 1
+            return kside or qside
         if side == "K":
-            side_index = WHITE_KSIDE_CASTLE_SHIFT
-        else: # == "Q"
-            side_index = WHITE_QSIDE_CASTLE_SHIFT
-        return self.board[8, side_index, 0] == 1
+            return kside
+        else:
+            return qside 
     
     def set_white_no_castle(self, type):
         """
+        (char) -> None
+
         A: for no castling at all
         K: for no kingside
         Q: for no queenside
         """
         if type == "A":
-            self.board[8,WHITE_KSIDE_CASTLE_SHIFT, 0] = 0
-            self.board[8,WHITE_QSIDE_CASTLE_SHIFT,0] = 0
+            self.board[HEURISTICS_LAYER, WHITE_KSIDE_CASTLE_INDEX[0], WHITE_KSIDE_CASTLE_INDEX[1]] = 0
+            self.board[HEURISTICS_LAYER, WHITE_KSIDE_CASTLE_INDEX[0], WHITE_KSIDE_CASTLE_INDEX[1]] = 0
         elif type == "K":
-                self.board[8, WHITE_KSIDE_CASTLE_SHIFT, 0] = 0 # set the white kside to 0
+                self.board[HEURESTICS_LAYER, WHITE_KSIDE_CASTLE_INDEX[0], WHITE_KSIDE_CASTLE_INDEX[1]] = 0 # set the white kside to 0
         else:
-            self.board[8,WHITE_QSIDE_CASTLE_SHIFT,0] = 0
-
-    
+            self.board[HEURESTICS_LAYER,WHITE_KSIDE_CASTLE_INDEX[0],WHITE_KSIDE_CASTLE_INDEX[1]] = 0
 
     def black_can_castle(self, side):
         """
+        (char) -> None
+
         A for either side
         K for kingside, Q for Q side
         """
+        kside = self.board[HEURISTICS_LAYER, BLACK_KSIDE_CASTLE_INDEX[0], BLACK_KSIDE_CASTLE_INDEX[1]] == 1
+        qside = self.board[HEURISTICS_LAYER, BLACK_QSIDE_CASTLE_INDEX[0], BLACK_QSIDE_CASTLE_INDEX[1]] == 1
         if side == "A":
-            # want to know if you can castle on either side
-            return self.board[8, BLACK_KSIDE_CASTLE_SHIFT, 0] == 1 or self.board[8, BLACK_QSIDE_CASTLE_SHIFT, 0] == 1
+            return kside or qside
         if side == "K":
-            side_index = BLACK_KSIDE_CASTLE_SHIFT
+            return kside
         else:
-            side_index = BLACK_QSIDE_CASTLE_SHIFT
-        return self.board[8, side_index, 0] == 1
+            return qside 
     
     def set_black_no_castle(self, side):
         """
@@ -323,10 +308,69 @@ class Board:
         K: for no kingside
         Q: for no queenside
         """
-        if side == "A":
-            self.board[8,BLACK_KSIDE_CASTLE_SHIFT, 0] = 0
-            self.board[8,BLACK_QSIDE_CASTLE_SHIFT,0] = 0
-        elif side == "K":
-            self.board[8, BLACK_KSIDE_CASTLE_SHIFT, 0] = 0 # set the white kside to 0
+        if type == "A":
+            self.board[HEURISTICS_LAYER, BLACK_KSIDE_CASTLE_INDEX[0], BLACK_KSIDE_CASTLE_INDEX[1]] = 0
+            self.board[HEURISTICS_LAYER, BLACK_KSIDE_CASTLE_INDEX[0], BLACK_KSIDE_CASTLE_INDEX[1]] = 0
+        elif type == "K":
+                self.board[HEURESTICS_LAYER, BLACK_KSIDE_CASTLE_INDEX[0], BLACK_KSIDE_CASTLE_INDEX[1]] = 0 # set the white kside to 0
         else:
-            self.board[8,BLACK_QSIDE_CASTLE_SHIFT,0] = 0
+            self.board[HEURESTICS_LAYER,BLACK_KSIDE_CASTLE_INDEX[0],BLACK_KSIDE_CASTLE_INDEX[1]] = 0
+    
+    @staticmethod
+    def parse_index(index):
+        '''
+        "a1" --> (0, 0)
+        '''
+        # TODO: Assert length = 2
+        return (int(index[1]) - 1, LETTER_TO_ROW[index[0]])
+
+    @staticmethod
+    def get_piece_layer_from_index(board, row, col):
+        """
+        (int, int) -> layer the piece resides in, independent of colour.
+
+        Returns None if no piece found
+        """
+        # Here we're going to have to traverse through the 6 piece layers
+        for layer in range(0, LAST_LAYER + 1): # because the knight is the last layer
+            if board[layer, row, col] != 0:
+                # Then the piece resides here
+                return layer
+        return None
+
+    @staticmethod
+    def get_piece_from_index(board, row, col):
+        '''
+        (row, col) -> str
+        '''
+        # (Exception propogates)
+        # get the layer
+        piece_layer = Board.get_piece_layer_from_index(board, row, col)
+        if piece_layer == None:
+            return None
+        piece = board[piece_layer, row, col]
+        if piece == WHITE_PRESENT:
+            return WHITE_LAYER_TO_PIECE[piece_layer]
+        else:
+            return BLACK_LAYER_TO_PIECE[piece_layer]
+
+    
+    @staticmethod
+    def parse_move_indices(move):
+        '''
+        returns the index for start piece, target piece
+        --> [(s_row, s_col), (t_row, t_col)]
+        NOTE: only takes the first chars for the indices, promotions and mate not considered
+        '''
+        move_str = str(move)
+        start_sq = move_str[0:2]
+        target_sq = move_str[2:4]
+        # g1 should be 0, 6
+        start_index = Board.parse_index(start_sq)
+        target_index = Board.parse_index(target_sq)
+        return [start_index, target_index]
+
+
+board = Board()
+
+print(board)
