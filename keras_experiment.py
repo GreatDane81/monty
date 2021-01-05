d1e778bae2507f760bb8a5410346bd7693ed89ce
Ethan Dain <ethan.dain@gmail.com>
2021-01-05 16:27:48 -0500

    cleaning the directory

--- keras_experiment.py
+++
@@ -1,8 +0,0 @@
-from tensorflow import keras
-
-model = keras.Sequential([
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(1, activation='sigmoid')])
-
